5f9d3ceb5edaef560b36d0ffa84cd5da2c2ec6c3
somkiat <somkiat.p@gmail.com>
2016-11-08 19:09:58 +0700

    Refactor code

--- DemoNetwork/DemoAlamofireAPI.swift
+++ DemoNetwork/DemoAlamofireAPI.swift
@@ -4,55 +4,37 @@ import Alamofire
 class DemoAlamorfireAPI {
     
     func process1() -> String {
-        let semaphore = dispatch_semaphore_create(0)
-        var result = "ERROR"
-        Alamofire.request(.GET, "http://localhost:8882/step1", parameters: nil).response(completionHandler: { request, response, data, error in
-            if response!.statusCode == 200 {
-                result = String(data: data!, encoding: NSUTF8StringEncoding)!
-            }
-            dispatch_semaphore_signal(semaphore)
-            
-        })
-        while dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW) != 0 {
-            NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 10))
-        }
-        print(result)
-        return result
+        let x=Alamofire.request(.GET, "http://localhost:8882/step1", parameters: nil)
+        return x.response2()
     }
     
     func process2() -> String {
-        let semaphore = dispatch_semaphore_create(0)
-        var result = "ERROR"
-        Alamofire.request(.GET, "http://localhost:8882/step2", parameters: nil).response(completionHandler: { request, response, data, error in
-            if response!.statusCode == 200 {
-                result = String(data: data!, encoding: NSUTF8StringEncoding)!
-            }
-            dispatch_semaphore_signal(semaphore)
-            
-        })
-        while dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW) != 0 {
-            NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 10))
-        }
-        print(result)
-        return result
+        let x=Alamofire.request(.GET, "http://localhost:8882/step2", parameters: nil)
+        return x.response2()
     }
     
     func process3() -> String {
-        let semaphore = dispatch_semaphore_create(0)
+        let x=Alamofire.request(.GET, "http://localhost:8882/step3", parameters: nil)
+        return x.response2()
+    }
+    
+    
+}
+
+extension Request {
+    public func response2() -> String {
         var result = "ERROR"
-        Alamofire.request(.GET, "http://localhost:8882/step3", parameters: nil).response(completionHandler: { request, response, data, error in
+        let semaphore = dispatch_semaphore_create(0)
+        self.response(completionHandler: { request, response, data, error in
             if response!.statusCode == 200 {
                 result = String(data: data!, encoding: NSUTF8StringEncoding)!
             }
             dispatch_semaphore_signal(semaphore)
-            
         })
-        while dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW) != 0 {
+        while dispatch_semaphore_wait(semaphore,DISPATCH_TIME_NOW) != 0 {
             NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 10))
         }
-        print(result)
+
         return result
     }
-    
-    
 }
